c98274e83300df396a9bf3509b15a44f10860bbb
meriy100 <ttattataa@gmail.com>
2016-10-29 17:39:37 +0900

    normalized_distance

--- lib/levenshtein_ruby.rb
+++ lib/levenshtein_ruby.rb
@@ -1,5 +1,26 @@
 require "levenshtein_ruby/version"
 require "levenshtein_ruby/levenshtein_ruby"
 
-class LevenshteinRuby
+class Levenshtein
+  def normalized_distance(a1, a2, threshold=nil, options={})
+    size  = [a1.size, a2.size].max
+
+    if a1.size == 0 and a2.size == 0
+      0.0
+    elsif a1.size == 0
+      a2.size.to_f/size
+    elsif a2.size == 0
+      a1.size.to_f/size
+    else
+      if threshold
+        if d = self.distance(a1, a2, (threshold*size).to_i+1)
+          d.to_f/size
+        else
+          nil
+        end
+      else
+        self.distance(a1, a2).to_f/size
+      end
+    end
+  end
 end
