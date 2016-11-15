437e900541e216cd34252b78450180750f3167ab
meriy100 <ttattataa@gmail.com>
2016-11-15 15:43:45 +0900

    previous sizeof removed

--- ext/levenshtein_ruby/distance.c
+++ ext/levenshtein_ruby/distance.c
@@ -2,6 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int* get_point(int** double_pointer, int x, int y)
+{
+  return *(double_pointer + sizeof(int*) * x) + sizeof(int) * y;
+}
+
+void set_point(int** double_pointer, int x, int y, int value)
+{
+  *(*(double_pointer + sizeof(int*) * x) + sizeof(int) * y) = value;
+}
+
 int distance(const char* word1, const char* word2)
 {
   int i;
@@ -16,52 +26,53 @@ int distance(const char* word1, const char* word2)
   fprintf(stderr, "len1: %d\n", len1);
   fprintf(stderr, "len2: %d\n", len2);
 
-  /* int matrix[len1 + 1][len2 + 1]; */
   for (i = 0; i <= len1; i++) {
-    **(matrix + i*sizeof(int*)) = i;
-    fprintf(stderr, "%d\n", **(matrix + i*sizeof(int*)));
+    set_point(matrix, i, 0, i);
+    fprintf(stderr, "%d\n", *get_point(matrix, i, 0));
   }
+
   fprintf(stderr, "ok\n");
   for (i = 0; i <= len2; i++) {
-    matrix[0][i] = i;
-    *(*matrix + sizeof(int)*i) = i;
-    fprintf(stderr, "%d\n", *(*matrix + sizeof(int)*i));
+    set_point(matrix, 0, i, i);
+    fprintf(stderr, "%d\n", *get_point(matrix, 0, i));
   }
   fprintf(stderr, "ok\n");
-  for (i = 1; i <= len1; i++) {
-    int j;
-    char c1;
 
-    c1 = word1[i-1];
-    for (j = 1; j <= len2; j++) {
-      char c2;
-
-      c2 = word2[j-1];
-      if (c1 == c2) {
-        matrix[i][j] = matrix[i-1][j-1];
-      }
-      else {
-        int delete;
-        int insert;
-        int substitute;
-        int minimum;
-
-        delete = matrix[i-1][j] + 1;
-        insert = matrix[i][j-1] + 1;
-        substitute = matrix[i-1][j-1] + 1;
-        minimum = delete;
-        if (insert < minimum) {
-          minimum = insert;
-        }
-        if (substitute < minimum) {
-          minimum = substitute;
-        }
-        matrix[i][j] = minimum;
-      }
-    }
-  }
-  result = matrix[len1][len2];
-  free(matrix);
+  /* for (i = 1; i <= len1; i++) { */
+  /*   int j; */
+  /*   char c1; */
+  /*  */
+  /*   c1 = word1[i-1]; */
+  /*   for (j = 1; j <= len2; j++) { */
+  /*     char c2; */
+  /*  */
+  /*     c2 = word2[j-1]; */
+  /*     if (c1 == c2) { */
+  /*       matrix[i][j] = matrix[i-1][j-1]; */
+  /*     } */
+  /*     else { */
+  /*       int delete; */
+  /*       int insert; */
+  /*       int substitute; */
+  /*       int minimum; */
+  /*  */
+  /*       delete = matrix[i-1][j] + 1; */
+  /*       insert = matrix[i][j-1] + 1; */
+  /*       substitute = matrix[i-1][j-1] + 1; */
+  /*       minimum = delete; */
+  /*       if (insert < minimum) { */
+  /*         minimum = insert; */
+  /*       } */
+  /*       if (substitute < minimum) { */
+  /*         minimum = substitute; */
+  /*       } */
+  /*       matrix[i][j] = minimum; */
+  /*     } */
+  /*   } */
+  /* } */
+  /* result = matrix[len1][len2]; */
+  /* free(matrix); */
+  result = 1;
   return result;
 }
 
