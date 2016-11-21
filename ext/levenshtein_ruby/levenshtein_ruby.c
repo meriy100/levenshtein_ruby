da270bc8f38cd0ffd7964bfac0c876e8bfc39d29
meriy100 <ttattataa@gmail.com>
2016-11-21 15:55:43 +0900

    version 0.1.4

--- ext/levenshtein_ruby/levenshtein_ruby.c
+++ ext/levenshtein_ruby/levenshtein_ruby.c
@@ -7,59 +7,10 @@ VALUE cLevenshtein;
 
 VALUE distance_func(VALUE self, VALUE s_word1, VALUE s_word2)
 {
-    int i;
+
     char* word1 = StringValuePtr(s_word1);
     char* word2 = StringValuePtr(s_word2);
-    int len1 = strlen(word1);
-    int len2 = strlen(word2);
-    int result;
-    int** matrix;
-    matrix = (int**)malloc(len1 * sizeof(int));
-    for(i = 0; i < len1; i++) {
-      matrix[i] = (int)malloc(len2 * sizeof(int));
-    }
-    /* int matrix[len1 + 1][len2 + 1]; */
-    for (i = 0; i <= len1; i++) {
-        matrix[i][0] = i;
-    }
-    for (i = 0; i <= len2; i++) {
-        matrix[0][i] = i;
-    }
-    for (i = 1; i <= len1; i++) {
-        int j;
-        char c1;
-
-        c1 = word1[i-1];
-        for (j = 1; j <= len2; j++) {
-            char c2;
-
-            c2 = word2[j-1];
-            if (c1 == c2) {
-                matrix[i][j] = matrix[i-1][j-1];
-            }
-            else {
-                int delete;
-                int insert;
-                int substitute;
-                int minimum;
-
-                delete = matrix[i-1][j] + 1;
-                insert = matrix[i][j-1] + 1;
-                substitute = matrix[i-1][j-1] + 1;
-                minimum = delete;
-                if (insert < minimum) {
-                    minimum = insert;
-                }
-                if (substitute < minimum) {
-                    minimum = substitute;
-                }
-                matrix[i][j] = minimum;
-            }
-        }
-    }
-    result = matrix[len1][len2];
-    free(matrix);
-    return INT2NUM(result);
+    return INT2NUM(distance(word1, word2));
 }
 
 void Init_levenshtein_ruby()
